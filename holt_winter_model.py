c2b0987809e1c518787b01a4060f0759a71985ff
NAMAN NIMBLE <118067229+NAMANNIMBLE1@users.noreply.github.com>
2025-02-12 18:36:18 +0530

    Create holt_winter_model.py

---
+++ holt_winter_model.py
@@ -0,0 +1,26 @@
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing , Holt , ExponentialSmoothing
+
+def plot_forecast(forecast , forecast1 , forecast2):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x = train['Month'] , y = train['#Passengers'] , name = "train"))
+    fig.add_trace(go.Scatter(x = test['Month'] , y = test['#Passengers'] , name = 'test'))
+    fig.add_trace(go.Scatter(x = test['Month'] , y = forecast , name = "simple"))     
+    fig.add_trace(go.Scatter(x = test['Month'] , y = forecast1 , name = 'holt linear')) 
+    fig.add_trace(go.Scatter(x = test['Month'] , y = forecast2 , name = 'holt winter'))       
+    return fig.show()   
+
+
+model1 = Holt(train['#Passengers'])
+model_fit = model.fit()
+forecast1 = model_fit.forecast(len(test))
+
+model2 = ExponentialSmoothing(train['#Passengers'], trend='add', seasonal='add', seasonal_periods=12)
+model_fit2 = model2.fit(smoothing_level=0.2, smoothing_trend=0.2, smoothing_seasonal=0.2 , optimized=True)
+forecast2 = model_fit2.forecast(len(test))
+
+model = SimpleExpSmoothing(train['#Passengers'])
+model_fit = model.fit()
+forecast = model_fit.forecast(len(test))
+
+
+plot_forecast(forecast , forecast1 , forecast2)
