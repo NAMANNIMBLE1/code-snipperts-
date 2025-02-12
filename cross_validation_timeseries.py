ad10fc04da053f74b965c4e2f76c006c56278cea
NAMAN NIMBLE <118067229+NAMANNIMBLE1@users.noreply.github.com>
2025-02-12 18:28:21 +0530

    Create cross_validation_timeseries.py

---
+++ cross_validation_timeseries.py
@@ -0,0 +1,71 @@
+from sklearn.model_selection import KFold
+import plotly.graph_objects as go
+
+
+def plot_cross_val(n_splits: int,
+                   splitter_func,
+                   df: pd.DataFrame,
+                   title_text: str) -> None:
+  
+    """Function to plot the cross validation of various
+    sklearn splitter objects."""
+
+    split = 1
+    plot_data = []
+
+    for train_index, valid_index in splitter_func(n_splits=n_splits).split(df):
+        plot_data.append([train_index, 'Train', f'{split}'])
+        plot_data.append([valid_index, 'Test', f'{split}'])
+        split += 1
+
+    plot_df = pd.DataFrame(plot_data,
+                           columns=['Index', 'Dataset', 'Split'])\
+                           .explode('Index')
+
+    fig = go.Figure()
+    for _, group in plot_df.groupby('Split'):
+        fig.add_trace(go.Scatter(x=group['Index'].loc[group['Dataset'] == 'Train'],
+                                 y=group['Split'].loc[group['Dataset'] == 'Train'],
+                                 name='Train',
+                                 line=dict(color="blue", width=10)
+                                 ))
+        fig.add_trace(go.Scatter(x=group['Index'].loc[group['Dataset'] == 'Test'],
+                                 y=group['Split'].loc[group['Dataset'] == 'Test'],
+                                 name='Test',
+                                 line=dict(color="goldenrod", width=10)
+                                 ))
+
+    fig.update_layout(template="simple_white", font=dict(size=20),
+                      title_text=title_text, title_x=0.5, width=850,
+                      height=450, xaxis_title='Index', yaxis_title='Split')
+    
+    legend_names = set()
+    fig.for_each_trace(
+        lambda trace:
+        trace.update(showlegend=False)
+        if (trace.name in legend_names) else legend_names.add(trace.name))
+
+    return fig.show()
+  
+# Read in the data
+
+df['Month'] = pd.to_datetime(df['Month'])
+
+# Plot the cross validation
+plot_cross_val(n_splits=5,
+              splitter_func=KFold,
+              df=df,
+              title_text='Cross-Validation')
+
+
+# Import packages
+from sklearn.model_selection import TimeSeriesSplit
+
+# Plot the time series cross validation splits
+plot_cross_val(n_splits=5,
+               splitter_func=TimeSeriesSplit,
+               df=df,
+               title_text='Time Series Cross-Validation')
+
+
+
