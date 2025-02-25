800a5ee845cebc4661ee3ebfd59f4c9afe020760
NAMAN NIMBLE <118067229+NAMANNIMBLE1@users.noreply.github.com>
2025-02-25 21:34:13 +0530

    Update data_augmentation_cnn.py

--- data_augmentation_cnn.py
+++ data_augmentation_cnn.py
@@ -32,5 +32,59 @@ for batch in datagen.flow(input_batch , batch_size=1 , save_to_dir='directory_to
     if i == 4: # how much images to generate
         break
 
+# proper use in projects #################################################################################
+
+
+
+train_datagen = ImageDataGenerator(
+            rotation_range=40,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            fill_mode='nearest',
+            brightness_range=[0.2,1.0],
+            rescale=1./255
+        )
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+                    directory="C:\\Users\\nex20\\Downloads\\archive (2)\\train",
+                    target_size=(256,256),
+                    color_mode='rgb',
+                    batch_size=32,
+                    class_mode='categorical',
+                    shuffle=True,
+                    seed=None,
+                    save_to_dir=None,
+                    save_prefix='',
+                    save_format='jpeg',
+                    follow_links=False,
+                    subset=None,
+                    interpolation='nearest'
+                )
+
+
+validation_generator = test_datagen.flow_from_directory(
+                    directory="C:\\Users\\nex20\\Downloads\\archive (2)\\test",
+                    target_size=(256,256),
+                    color_mode='rgb',
+                    batch_size=32,
+                    class_mode='categorical',
+                    shuffle=True,
+                    seed=None,
+                    save_to_dir=None,
+                    save_prefix='',
+                    save_format='jpeg',
+                    follow_links=False,
+                    subset=None,
+                    interpolation='nearest'
+                )
+
+model.compile(optimizer='adam' , loss='categorical_crossentropy' , metrics=['accuracy'])
+model.fit(train_generator , validation_data=validation_generator , epochs=100 , batch_size=32 , callbacks= [early_stopping])
+
 
 
